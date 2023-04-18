4bd14ae63aea616fcdce43c9e5bc0a34963abce1
balatuluva <bala.tulva@gmail.com>
2023-04-18 19:34:54 +0100

    frontend script and config files

--- frontend.sh
+++ frontend.sh
@@ -4,5 +4,5 @@ rm -rf /usr/share/nginx/html/*
 cd /usr/share/nginx/html
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 unzip /tmp/frontend.zip
-systemctl restart nginx
+systemctl start nginx
 systemctl enable nginx
