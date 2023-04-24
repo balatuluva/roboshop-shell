df12961053ab2cde859bb8719c68513006228575
balatuluva <bala.tulva@gmail.com>
2023-04-24 20:43:18 +0100

    shipping final script

--- shipping.sh
+++ shipping.sh
@@ -1,35 +1,14 @@
-echo -e "\e[36m>>>>>>> Install maven <<<<<<<\e[0m"
-yum install maven -y
+script=$(realpath "$0")
+script_path=$(dirname "$script")
+source ${script_path}/common.sh
+mysql_root_password=$1
 
-echo -e "\e[36m>>>>>>> Add user roboshop <<<<<<<\e[0m"
-useradd roboshop
+if [ -z "$mysql_root_password" ]; then
+  echo Input MySQL Root Password Missing
+  exit
+fi
 
-echo -e "\e[36m>>>>>>> Create app Directory <<<<<<<\e[0m"
-rm -rf /app
-mkdir /app
-
-echo -e "\e[36m>>>>>>> Download app content <<<<<<<\e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-
-echo -e "\e[36m>>>>>>> Extract app content <<<<<<<\e[0m"
-cd /app
-unzip /tmp/shipping.zip
-
-echo -e "\e[36m>>>>>>> Download maven dependencies <<<<<<<\e[0m"
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-
-echo -e "\e[36m>>>>>>> Copy shipping repo file <<<<<<<\e[0m"
-cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
-
-echo -e "\e[36m>>>>>>> Install MySQL <<<<<<<\e[0m"
-yum install mysql -y
-
-echo -e "\e[36m>>>>>>> Load schema <<<<<<<\e[0m"
-mysql -h mysql.gehana26.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
-
-echo -e "\e[36m>>>>>>> Start shipping service <<<<<<<\e[0m"
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
+component="shipping"
+schema_setup=mysql
+func_java
 
