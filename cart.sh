99cc4a8b4fe13d2dbaa8c9d0173627e2e21a5651
balatuluva <bala.tulva@gmail.com>
2023-04-24 20:27:32 +0100

    cart final script

--- cart.sh
+++ cart.sh
@@ -2,15 +2,6 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd ${app_user}
-mkdir /app
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
-cd /app
-unzip /tmp/cart.zip
-npm install
-cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service
-systemctl daemon-reload
-systemctl enable cart
-systemctl restart cart
+component=cart
+
+func_nodejs
