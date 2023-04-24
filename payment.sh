a32c484ffb9439b91bffd3cbb4a2eab28ac53b23
balatuluva <bala.tulva@gmail.com>
2023-04-24 20:38:24 +0100

    payment final script

--- payment.sh
+++ payment.sh
@@ -1,26 +1,12 @@
-echo -e "\e[36m>>>>>>> Install Python <<<<<<<\e[0m"
-yum install python36 gcc python3-devel -y
+script=$(realpath "$0")
+script_path=$(dirname "$script")
+source ${script_path}/common.sh
+rabbitmq_appuser_password=$1
 
-echo -e "\e[36m>>>>>>> Add app user <<<<<<<\e[0m"
-useradd roboshop
+if [ -z "$rabbitmq_appuser_password" ]; then
+  echo Input Roboshop Appuser Password Missing
+  exit
+fi
 
-echo -e "\e[36m>>>>>>> Create app directory <<<<<<<\e[0m"
-mkdir /app
-
-echo -e "\e[36m>>>>>>> Download app content <<<<<<<\e[0m"
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
-
-echo -e "\e[36m>>>>>>> unzip app content <<<<<<<\e[0m"
-cd /app
-unzip /tmp/payment.zip
-
-echo -e "\e[36m>>>>>>> Install python dependencies <<<<<<<\e[0m"
-pip3.6 install -r requirements.txt
-
-echo -e "\e[36m>>>>>>> copy payment repos <<<<<<<\e[0m"
-cp /home/centos/roboshop-shell/payment.service /etc/systemd/system/payment.service
-
-echo -e "\e[36m>>>>>>> start payment service <<<<<<<\e[0m"
-systemctl daemon-reload
-systemctl enable payment
-systemctl restart payment
+component=payment
+func_python
